5aaf97f2d3c575e7ca0d2b97dc5e37a478eea7d9
lishijia <shijia.li@hotmail.com>
2018-12-07 20:01:15 +0800

    spark scala jieba hive集成 maven版本

--- src/main/scala/lishijia/streaming/scenic/kafka/KafkaApp.java
+++ src/main/scala/lishijia/streaming/scenic/kafka/KafkaApp.java
@@ -2,30 +2,38 @@ package lishijia.streaming.scenic.kafka;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * 模拟发送消息
+ */
 public class KafkaApp {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws InterruptedException {
         KafkaSenderProcessor processor = new KafkaSenderProcessor();
-        Order order = new Order();
-        order.setScenic_code("s0001");
-        order.setScenic_name("景区s0001");
-        order.setChannel_name("销售渠道c001");
-        order.setChannel_code("c001");
-        order.setOrder_no("s000100000001");
-        order.setPlace_time("2018-12-05 12:23:12");
-        order.setSettle_price("110.00");
-        order.setSettle_amount("110.00");
-        order.setCertificate_no("430502xxxx06176212");
-        order.setMobile_no("136xxxx6224");
-
         KafkaConfiguration configuration = new KafkaConfiguration();
         configuration.setHost("hadoop101:9092,hadoop102:9092,hadoop103:9092");
-
         processor.setKafkaConfiguration(configuration);
         processor.init();
-
-        processor.send(JSONObject.toJSONString(order), "orderTopic");
-
+        Random s = new Random();
+        for(int i=0;i<10000;i++){
+            Order order = new Order();
+            int code = (s.nextInt(9)+1);
+            order.setScenic_code("s000" + code);
+            order.setScenic_name("景区s000" + code);
+            order.setChannel_name("销售渠道c00" + code);
+            order.setChannel_code("c00" + code);
+            order.setOrder_no(UUID.randomUUID().toString().replace("-",""));
+            order.setPlace_time("2018-12-05 12:23:12");
+            order.setSettle_price("110.00");
+            order.setSettle_amount("110.00");
+            order.setCertificate_no("430502xxxx06176212");
+            order.setMobile_no("136xxxx6224");
+            Thread.sleep(1000);
+            //发送消息
+            processor.send(JSONObject.toJSONString(order), "orderTopic");
+        }
         System.out.println("done");
     }
 
